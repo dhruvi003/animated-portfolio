a44a572a3623d545ae48bd5220404b3c65df7544
dhruvi003 <cdhwani03@gmail.com>
2024-08-18 15:54:43 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".multiple-text",{
+    strings: ["FrontEnd Developer","Backend Developer","VAPT"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 500,
+    loop: true
+
+})
